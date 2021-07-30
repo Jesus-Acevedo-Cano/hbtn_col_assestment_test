1eb6f675fbb54a16ec087f87e9a75261d9a260e0
Jesus-Acevedo-Cano <jeacevedocano@gmail.com>
2021-07-30 16:43:06 -0500

    url definition to make it ordered

--- views/orders.py
+++ views/orders.py
@@ -1,16 +1,16 @@
 from django.shortcuts import render, HttpResponse
 
-def order(request, order_id):
-    return HttpResponse(f"pass the order ID and get the formatted JSON with the required information {order_id}")
+def order(request, orderid):
+    return HttpResponse(f"get the formatted JSON with the required information {orderid}")
 
 def orders(request):
-    return HttpResponse("pass multiple order IDs in string format, separated by comma (,) and return the info of those orders.")
+    return HttpResponse("pass multiple order IDs and return the info of those orders.")
 
 def dates(request):
     return HttpResponse("pass two dates and return the ids of the orders between those dates")
 
 def shipping(request):
-    return HttpResponse("return all orders with the given key (city, state, country) and string as value.")
+    return HttpResponse("return all orders with the given key and string as value.")
 
 def search(request):
-    return HttpResponse("search for the term given in multiple parts of the order, and return the results")
+    return HttpResponse("search for the term given, and return the results")
